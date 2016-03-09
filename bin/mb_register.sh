2c6f1dfeba260f6c5b6ceb1fcb1b3fcfd81663c5
Gabriel A. Devenyi <gdevenyi@gmail.com>
2016-03-09 15:42:12 -0500

    Update registration to be more similar to classic magetbrain, add alterate versions based on antsRegsitation scripts

--- bin/mb_register.sh
+++ bin/mb_register.sh
@@ -9,9 +9,9 @@ antsRegistration --dimensionality 3 --float 0 --collapse-output-transforms 1 --v
   --output [$outputdir/$(basename $movingfile)-$(basename $fixedfile)] \
   --winsorize-image-intensities [0.01,0.99] --use-histogram-matching 1 \
   --initial-moving-transform [$fixedfile,$movingfile,1] \
-  --transform Rigid[0.5] --metric Mattes[$fixedfile,$movingfile,1,32,Random,0.5] --convergence [2000x2000x2000x2000x0,1e-6,10] --shrink-factors 16x8x4x2x1 --smoothing-sigmas 4x3x2x1x0vox --masks [NULL,NULL] \
-  --transform Affine[0.1] --metric Mattes[$fixedfile,$movingfile,1] --convergence [10000x10000x10000x10000,1e-6,10] --shrink-factors 8x4x2x1 --smoothing-sigmas 3x2x1x0vox --masks [$fixedmask,$movingmask] \
-  --transform BSplineSyN[0.5,52,0] --metric CC[$fixedfile,$movingfile,1,4] --convergence [400x200x100x50x25,1e-6,10] --shrink-factors 16x8x4x2x1 --smoothing-sigmas 5x4x2x1x0vox --masks [NULL,NULL]
-
+  --transform Rigid[0.1] --metric Mattes[$fixedfile,$movingfile,1] --convergence [2000,1e-6,10] --shrink-factors 8 --smoothing-sigmas 6.794mm \
+  --transform Similarity[0.1] --metric Mattes[$fixedfile,$movingfile,1] --convergence [2000,1e-6,10] --shrink-factors 4 --smoothing-sigmas 3.397mm \
+  --transform Affine[0.1] --metric Mattes[$fixedfile,$movingfile,1] --convergence [2000x2000x2000,1e-6,10] --shrink-factors 4x4x2 --smoothing-sigmas 3.397x1.698x0.849mm \
+  --transform SyN[0.5,3,0] --metric CC[$fixedfile,$movingfile,1,4] --convergence [400x400x400x200x25,1e-6,10] --shrink-factors 12x8x4x2x1 --smoothing-sigmas 5x4x2x1x0vox && \
+    rm $outputdir/$(basename $movingfile)-$(basename $fixedfile)*inverse*
 #Inverses are never used, remove them right after creation (if only I could disable creation...)
-rm $outputdir/$(basename $movingfile)-$(basename $fixedfile)*inverse*
