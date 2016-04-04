3daedc8b8eda2ee7b5b4deb1f4db30a5fd92d53b
Gabriel A. Devenyi <gdevenyi@gmail.com>
2016-04-04 15:29:32 -0400

    Add subject-wise cross validation collection scripts

---
+++ bin/nfold_subject_cv_collect.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [[ ! -n $1 ]]
+then
+    echo "Output file not specified"
+    exit 1
+fi
+
+output=$1
+echo "file,atlases,templates,fold,Label,Total/Target,Jaccard,Dice,VolumeSimilarity,FalseNegative,FalsePositive" > $output
+for dir in NFOLDCV/*
+do
+    atlases=$(basename $dir | grep -Eho '[0-9]+atlases' | sed 's/atlases//g')
+    templates=$(basename $dir | grep -Eho '[0-9]+templates' | sed 's/templates//g')
+    fold=$(basename $dir | grep -Eho 'fold[0-9]+' | sed 's/fold//g')
+    for label in $dir/*/output/labels/majorityvote/*.mnc
+    do
+        cat <<-EOT
+        LabelOverlapMeasures 3 input/atlas/$(basename $label | sed 's#_t1.mnc##g') $label >(tail -n +2) | awk -vT="$(basename $label),$atlases,$templates,$fold," '{ print T \$0 }' >> $output
+EOT
+    done | parallel -v
+done
