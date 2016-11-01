d22ce561342e68f7aed5c298b36100a4510cddff
Gabriel A. Devenyi <gdevenyi@gmail.com>
2016-11-01 13:26:53 -0400

    Remove old morpho test bits

--- bin/mb_resample.sh
+++ bin/mb_resample.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #mb_resample.sh labelname atlasname templatename subjectname
 set -euo pipefail
-IFS=$'\n\t'
 
 export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=${THREADS_PER_COMMAND:-$(nproc)}
 
@@ -13,23 +12,12 @@ subjectname=$(basename $subject)
 atlasname=$(basename $atlas)
 templatename=$(basename $template)
 
-#Here, we do a check for the presence of morpho transforms, if they exist
-#If they do, push a transform to the end of the transform stack
-if [[ -s output/transforms/modelspace/${subjectname}0_GenericAffine.xfm ]]
-then
-    morphosubjecttransform=" -t [output/transforms/modelspace/${subjectname}0_GenericAffine.xfm,1]"
-else
-    morphosubjecttransform=""
-fi
-
-
 #Check for subjectname == $templatename, if so, we skipped that registration, so don't apply those transforms
 if [[ ${subjectname} = ${templatename} ]]
 then
 antsApplyTransforms -d 3  ${MB_VERBOSE:-} --interpolation GenericLabel -r ${subject} \
     -i $(echo $atlas | sed -E "s/t1\.(nii|nii\.gz|mnc)/${labelname}/g") \
     -o /tmp/${atlasname}-${templatename}-${subjectname}-$labelname \
-    ${morphosubjecttransform} \
     -t output/transforms/atlas-template/${templatename}/${atlasname}-${templatename}1_NL.xfm \
     -t output/transforms/atlas-template/${templatename}/${atlasname}-${templatename}0_GenericAffine.xfm
 else
@@ -37,7 +25,6 @@ else
 antsApplyTransforms -d 3  ${MB_VERBOSE:-} --interpolation GenericLabel -r ${subject} \
     -i $(echo $atlas | sed -E "s/t1\.(nii|nii\.gz|mnc)/${labelname}/g") \
     -o /tmp/${atlasname}-${templatename}-${subjectname}-$labelname \
-    ${morphosubjecttransform} \
     -t output/transforms/template-subject/${subjectname}/${templatename}-${subjectname}1_NL.xfm \
     -t output/transforms/template-subject/${subjectname}/${templatename}-${subjectname}0_GenericAffine.xfm \
     -t output/transforms/atlas-template/${templatename}/${atlasname}-${templatename}1_NL.xfm \
