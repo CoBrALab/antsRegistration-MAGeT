b36d4c6e892679c34c640434c491022162d6e1b6
Gabriel A. Devenyi <gdevenyi@gmail.com>
2017-02-06 17:27:15 -0500

    Fix sed call

--- bin/stages.sh
+++ bin/stages.sh
@@ -251,9 +251,9 @@ stage_qc () {
       labelname=$(basename ${label})
       if [[ ! -s output/labels/QC/${subjectname}_${labelname}.jpg ]]
       then
-        echo mb_qc.sh ${subject} output/labels/majorityvote/${subjectname}_$(echo $labelname | sed -E 's/(.mnc|.nii|.nii.gz|.nrrd)//g')$(echo $subjectname | grep -i -o -E '(.mnc|.nii|.nii.gz|.nrrd)') \
+        echo mb_qc.sh ${subject} output/labels/majorityvote/${subjectname}_$(echo $labelname | sed -r 's/(.mnc|.nii|.nii.gz|.nrrd)//g')$(echo $subjectname | grep -i -o -E '(.mnc|.nii|.nii.gz|.nrrd)') \
             output/labels/QC
-        debug mb_qc.sh ${subject} output/labels/majorityvote/${subjectname}_$(echo $labelname | sed -E 's/(.mnc|.nii|.nii.gz|.nrrd)//g')$(echo $subjectname | grep -i -o -E '(.mnc|.nii|.nii.gz|.nrrd)') \
+        debug mb_qc.sh ${subject} output/labels/majorityvote/${subjectname}_$(echo $labelname | sed -r 's/(.mnc|.nii|.nii.gz|.nrrd)//g')$(echo $subjectname | grep -i -o -E '(.mnc|.nii|.nii.gz|.nrrd)') \
             output/labels/QC
       fi
     done | qbatch ${dryrun} -j 2 -c 1000 --depend ${datetime}-mb_vote-${subjectname} --jobname ${datetime}-mb_qc-${subjectname} --walltime 0:30:00 -
