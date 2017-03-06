70cf32c75692202cc2bb0f6db7ceab893606eeb7
Gabriel A. Devenyi <gdevenyi@gmail.com>
2017-03-06 09:47:07 -0500

    Wrap end-of-line code for heredoc so variables don't expand

--- bin/mb-container.sh
+++ bin/mb-container.sh
@@ -8,7 +8,7 @@ singularity exec -B $(pwd):/scratch --contain ${container} sh -c "unset PYTHONHO
 
 for job in $(ls -tr magetbrain-container-jobs/*meta)
 do
-    qbatch -n --header "read -r -d '' singularity_script << EOM" --header "unset PYTHONHOME; unset PYTHONPATH;" \
+    qbatch -n --header "read -r -d '' singularity_script << 'EOM'" --header "unset PYTHONHOME; unset PYTHONPATH;" \
 	--header "cd /scratch" \
         --footer "EOM" --footer "singularity exec -B $(pwd):/scratch --contain ${container} sh -c \"\$singularity_script\"" \
 	$(cat $job) $(dirname $job)/$(basename $job .meta).joblist
