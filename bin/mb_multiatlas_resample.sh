bee383a97ce3e1d52fe749cba9ad61ef06516ab3
Gabriel A. Devenyi <gdevenyi@gmail.com>
2018-08-13 14:53:57 -0400

    Linter reformatting

--- bin/mb_multiatlas_resample.sh
+++ bin/mb_multiatlas_resample.sh
@@ -21,11 +21,11 @@ atlasname=$(basename $atlas)
 templatename=$(basename $template)
 
 #Transforms are applied like matrix algebra, last transform on the command line is applied first
-antsApplyTransforms -d 3 ${MB_VERBOSE:-} --interpolation GenericLabel -r ${template} \
-    -i $(echo $atlas | sed -r 's/(t1|T1w|t2|T2w).*//g')${labelname} \
-    -o /tmp/${atlasname}-${templatename}-${labelname} \
-    -t output/transforms/atlas-template/${templatename}/${atlasname}-${templatename}1_NL.xfm \
-    -t output/transforms/atlas-template/${templatename}/${atlasname}-${templatename}0_GenericAffine.xfm
+antsApplyTransforms -d 3 ${__mb_float} ${MB_VERBOSE:-} --interpolation GenericLabel -r ${template} \
+  -i $(echo $atlas | sed -r 's/(t1|T1w|t2|T2w).*//g')${labelname} \
+  -o /tmp/${atlasname}-${templatename}-${labelname} \
+  -t output/transforms/atlas-template/${templatename}/${atlasname}-${templatename}1_NL.xfm \
+  -t output/transforms/atlas-template/${templatename}/${atlasname}-${templatename}0_GenericAffine.xfm
 
 ConvertImage 3 /tmp/${atlasname}-${templatename}-${labelname} output/multiatlas/labels/candidates/${templatename}/${atlasname}-${templatename}-${labelname} 1
 rm -f /tmp/${atlasname}-${templatename}-${labelname}
